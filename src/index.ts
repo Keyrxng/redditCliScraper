d1670c3e6153a3179763713f9570ec686b0ee400
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2023-09-30 14:25:33 +0100

    chore: asicci
    new intro

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,18 @@ import * as path from 'path';
 // Path to the directory containing command modules.
 const commandsPath = path.join(__dirname, 'commands');
 
-const cli = new CLI('r/Scraper', commandsPath);
+const cli = new CLI('r/Scraped', commandsPath);
 
 const shim = new Shim(cli);
 shim.execute(process.argv);
+
+console.log(`
+██████╗     ██╗███████╗ ██████╗██████╗  █████╗ ██████╗ ███████╗██████╗ 
+██╔══██╗   ██╔╝██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+██████╔╝  ██╔╝ ███████╗██║     ██████╔╝███████║██████╔╝█████╗  ██║  ██║
+██╔══██╗ ██╔╝  ╚════██║██║     ██╔══██╗██╔══██║██╔═══╝ ██╔══╝  ██║  ██║
+██║  ██║██╔╝   ███████║╚██████╗██║  ██║██║  ██║██║     ███████╗██████╔╝
+╚═╝  ╚═╝╚═╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚══════╝╚═════╝ 
+`)
+
 
